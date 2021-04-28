f27b1750153dd7faf80cf61dd72556259503d591
Daniel Ting <danielzting@gmail.com>
2021-04-27 22:07:55 -0500

    Wait until page load to replace
    The extension was breaking pages that used certain JavaScript libraries
    like Angular. For example, it would cause all projects to disappear on
    the Overleaf home page.
    
    My hypothesis is that Angular dynamically modifies the DOM depending on
    its template syntax (dunno how it works exactly) so editing text before
    it finishes loading messes up its templates.

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// HACK: Uses zero-width Unicode characters to remember its original
-// capitalization when modifying the clipboard
-
 function substitute(node) {
     if (
         node.parentNode !== null
@@ -9,15 +6,18 @@ function substitute(node) {
         && node.parentNode.tagName !== 'NOSCRIPT'
         && !node.parentNode.isContentEditable
     ) {
+        // HACK: Use zero-width Unicode characters to remember the
+        // original capitalization when modifying the clipboard
         node.nodeValue = node.nodeValue.replace(/B/g, '🅱️\u200c').replace(/b/g, '🅱️\u200d');
     }
 }
 
+// Bind listener for page changes
 var observer = new MutationSummary({
     callback: function(summaries) {
         var url = new URL(location.href).origin;
         chrome.storage.sync.get([url], function(data) {
-            if (!data[url]) {
+            if (data[url] === undefined) {
                 summaries[0].added.forEach(substitute);
                 summaries[0].valueChanged.forEach(substitute);
             }
@@ -28,10 +28,16 @@ var observer = new MutationSummary({
 
 var url = new URL(location.href).origin;
 chrome.storage.sync.get([url], function(data) {
-    if (!data[url]) {
+    if (data[url] === undefined) {
+        // Override clipboard handler to restore replaced text
         document.addEventListener('copy', function(event) {
             event.clipboardData.setData('text/plain', document.getSelection().toString().replace(/🅱️\u200c/gu, 'B').replace(/🅱️\u200d/gu, 'b'));
             event.preventDefault();
         });
+        // Perform an initial round of substitution after page load
+        var nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+        while (nodes.nextNode()) {
+            substitute(nodes.currentNode);
+        }
     }
 });
