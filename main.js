53a52436f7749e3d0b4311c0d430d529c03a3839
Daniel Ting <danielzting@gmail.com>
2021-07-16 22:19:47 -0500

    Add hack to ignore text from icon fonts
    Doesn't work for the icons inside the initial viewport after
    https://fonts.google.com/icons has finished loading because it seems
    like they change the font dynamically after the elements are loaded in
    for some reason. I don't this is solvable and I don't think this is
    common enough to warrant further attention.

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-function substitute(node) {
+function replaceNode(node) {
     if (
         node.parentNode !== null
         && node.parentNode.tagName !== 'SCRIPT'
         && node.parentNode.tagName !== 'STYLE'
         && node.parentNode.tagName !== 'NOSCRIPT'
+        // Ignore user-editable elements as manipulating them resets the cursor position
         && !node.parentNode.isContentEditable
+        // Workaround to avoid breaking icon fonts
+        && !window.getComputedStyle(node.parentNode).getPropertyValue('font-family').toUpperCase().includes("ICON")
     ) {
-        // HACK: Use zero-width Unicode characters to remember the
-        // original capitalization when modifying the clipboard
+        // HACK: Use Unicode zero-width characters to preserve case when copying to clipboard
         node.nodeValue = node.nodeValue.replace(/B/g, '🅱️\u200c').replace(/b/g, '🅱️\u200d');
     }
 }
 
-// Bind listener for page changes
-var observer = new MutationSummary({
-    callback: function(summaries) {
-        var url = new URL(location.href).origin;
-        chrome.storage.sync.get([url], function(data) {
-            if (data[url] === undefined) {
-                summaries[0].added.forEach(substitute);
-                summaries[0].valueChanged.forEach(substitute);
-            }
-        });
-    },
-    queries: [{ characterData: true }]
-});
-
-var url = new URL(location.href).origin;
+// Check if extension is enabled for current page
+let url = new URL(location.href).origin;
 chrome.storage.sync.get([url], function(data) {
     if (data[url] === undefined) {
-        // Override clipboard handler to restore replaced text
+        // Override clipboard handler to restore modified text with original
         document.addEventListener('copy', function(event) {
-            event.clipboardData.setData('text/plain', document.getSelection().toString().replace(/🅱️\u200c/gu, 'B').replace(/🅱️\u200d/gu, 'b'));
+            event.clipboardData.setData('text/plain', window.getSelection().toString().replace(/🅱️\u200c/gu, 'B').replace(/🅱️\u200d/gu, 'b'));
             event.preventDefault();
         });
-        // Perform an initial round of substitution after page load
-        var nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+
+        // Set up observer for future page changes
+        let observer = new MutationSummary({
+            callback: function(summaries) {
+                summaries[0].added.forEach(replaceNode);
+                summaries[0].valueChanged.forEach(replaceNode);
+            },
+            queries: [{ characterData: true }],
+        });
+
+        // Perform an initial round of substitution
+        let nodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
         while (nodes.nextNode()) {
-            substitute(nodes.currentNode);
+            replaceNode(nodes.currentNode);
         }
     }
 });
